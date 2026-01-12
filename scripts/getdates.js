2df9d856e2cbf1a97ddf36d41e7862c017c1d886
ramirotl <stapialoza@byupathway.edu>
2026-01-12 02:03:24 -0400

    w1 project, best attempt so far

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
