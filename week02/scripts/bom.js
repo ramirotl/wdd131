edc545efc166478a81bee3ade908ee41c074d506
ramirotl <stapialoza@byupathway.edu>
2026-01-16 18:08:42 -0400

    bom Top chapters app

---
+++ week02/scripts/bom.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list')
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+        li.textContent = input.value;
+
+        li.append(deleteButton);
+
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.value = '';
+    }
+
+    input.focus();
+});
+
