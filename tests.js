f0a9dc5ebc21e1f1b4c31b7d1f048a1cc7c9592b
ramirotl <stapialoza@byupathway.edu>
2026-01-22 19:00:53 -0400

    tests in js and created Country Page files for w03

--- tests.js
+++ tests.js
@@ -2,3 +2,14 @@ let myNumber = "500"; // oops, this is still a string
 typeof myNumber;
 myNumber = 500; // much better — now this is a number
 typeof myNumber;
+
+let firstName = 'Antonia';
+let lastName = 'Francesca';
+
+function fullName(first, last) {
+    return `${first} ${last}`;  //first +''+ last
+}
+
+const fullName = function (first, last) {
+    return `${first} ${last}`;
+}
