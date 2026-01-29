5defab594e35cf1108979ffeb964840ea956c8a9
ramirotl <stapialoza@byupathway.edu>
2026-01-29 10:14:57 -0400

    update place page and lazy loading

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,6 @@
+//Footer
+const year = document.querySelector("#currentyear");
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
